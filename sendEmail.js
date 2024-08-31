faef57b09df54196a2e232d20e206473be997d95
reuht <jhosephvillalba@gmail.com>
2024-08-31 10:18:40 -0500

    servicio de correo

---
+++ sendEmail.js
@@ -0,0 +1,24 @@
+//  listen to the form submission
+ document
+ .getElementById("myForm")
+ .addEventListener("submit", function (event) {
+   event.preventDefault();
+
+   const serviceID = "service_cm8n8on";
+   const templateID = "template_5nx0n2r";
+   const public_key = "4zJZIzjY1xmA8iB4L"
+
+   // Obtener el correo del usuario
+   emailjs.init(public_key); 
+   // send the email here
+   emailjs.sendForm(serviceID, templateID, this).then(
+     (response) => {
+       console.log("SUCCESS!", response.status, response.text);
+       alert("SUCCESS!");
+     },
+     (error) => {
+       console.log("FAILED...", error);
+       alert("FAILED...", error);
+     }
+   );
+ });
