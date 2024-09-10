8bcacb04d62f166f6094e29a5d6ce2915346e46e
Diego Parra <diego10.dapj@gmail.com>
2024-09-10 01:35:11 -0500

    cambios finales

--- buttonScript.js
+++ buttonScript.js
@@ -23,3 +23,29 @@ document
     document.querySelector('.input-container1').style.display = 'flex';
     document.querySelector('#messageInput1').focus();
 });
+
+
+
+
+
+/* Segunda parte para el btn del footer */
+
+
+// Función para manejar el botón del footer
+function sendMessageFooter() {
+    const inputFooter = document.querySelector('#messageInputFooter');
+    const successMessageFooter = document.querySelector('#successMessageFooter');
+  
+    if (inputFooter.value.trim() !== '') {
+      successMessageFooter.style.display = 'block';
+      document.querySelector('.input-container-footer').style.display = 'none';
+    } else {
+      alert('Please enter a message before sending.');
+    }
+  }
+  
+  document.getElementById("btn1-footer").addEventListener("click", function (event) {
+    document.querySelector('.button-container-footer').style.display = 'none';
+    document.querySelector('.input-container-footer').style.display = 'flex';
+    document.querySelector('#messageInputFooter').focus();
+  });
